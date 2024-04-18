7ac86e148c73632e7dcbf6f3fceca93b68b8f386
ksr0506 <ksr25160@naver.com>
2024-04-18 19:55:51 +0900

    Update and rename Shell/setStaticIP.bat to Shell/Batch/setStaticIP.bat

--- Shell/Batch/setStaticIP.bat
+++ Shell/Batch/setStaticIP.bat
@@ -1,9 +1,9 @@
 SET NICName=Wi-Fi
-SET IP=10.225.123.123
-SET SUBNETMASK=255.255.255.0
-SET GATEWAY=10.225.123.1
-SET DNS1=147.6.44.44
-SET DNS2=147.6.44.45
+SET IP=0.0.0.0
+SET SUBNETMASK=0.0.0.0
+SET GATEWAY=0.0.0.0
+SET DNS1=0.0.0.0
+SET DNS2=0.0.0.0
 
 netsh -c int ip set address name=%NICName% source=static addr=%IP% mask=%SUBNETMASK% gateway=%GATEWAY% gwmetric=0
 netsh -c int ip set dnsservers name=%NICName% source=static addr=%DNS1% register=primary no
