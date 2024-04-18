3efc4fd4804cdf647c26b1600cab40b7bf13aec6
ksr0506 <ksr25160@naver.com>
2024-04-18 19:53:56 +0900

    Update and rename Shell/setDHCP.bat to Shell/Batch/setDHCP.bat

---
+++ Shell/Batch/setDHCP.bat
@@ -0,0 +1,2 @@
+netsh -c int ip set address name="Wi-Fi" source=dhcp
+netsh -c int ip set dnsservers name="Wi-Fi" source=dhcp
--- Shell/setDHCP.bat
+++
@@ -1,2 +0,0 @@
-netsh -c int ip set address name="Wi-Fi" source=dhcp
-netsh -c int ip set dnsservers name="Wi-Fi" source=dhcp
