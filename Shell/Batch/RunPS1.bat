091c3bc415c3629acf3a7a50c9cfee81b85d60ff
ksr0506 <ksr25160@naver.com>
2024-04-18 21:17:53 +0900

    Update RunPS1.bat

--- Shell/Batch/RunPS1.bat
+++ Shell/Batch/RunPS1.bat
@@ -1,4 +1,4 @@
-::실행되는 .bat파일의 같은 경로에 같은 이름으로 존재하는 ps1파일을 실행시켜줍니다.
-::.bat파일의 이름이 RunPs1.bat이면, 같은 경로에 존재하는 RunPs1.ps1 파일을 실행 시켜줍니다.
+::실행되는 Batch File(*.bat)의 같은 경로에 같은 이름으로 존재하는 PowerShell Script(*.ps1)를 실행시켜줍니다.
+::Batch File(*.bat)의 이름이 RunPs1.bat이면, 같은 경로에 존재하는 RunPs1.ps1을 실행 시켜줍니다.
 ::powershell.exe "~/RunPs1.ps1"
-powershell.exe "%~dp0%~n0.ps1"
+powershell.exe -ExecutionPolicy Remotesigned -File "%~dp0%~n0.ps1"
